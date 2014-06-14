69caed0e64bb2f0d10b481a1c0e23be00df59d24
Balthazar Gronon <bgronon@gmail.com>
2014-06-14 19:51:26 +0200

    keep working

--- js/app.js
+++ js/app.js
@@ -13,8 +13,13 @@ client.on('data', function (data) {
 	console.log(res);
 });
 
+//Error caught
+process.on('uncaughtException', function (e) {
+	console.log(e);
+});
+
 angular.module('zabylonApp', [])
-	.controller('zabylonCtrl', function ($scope) {
+	.controller('zabylonCtrl', function ($scope, $timeout) {
 
 		$scope.ui = {
 			connected  : false,
@@ -25,6 +30,24 @@ angular.module('zabylonApp', [])
 		$scope.connect = function () {
 			client.connect($scope.ui.connectPort, $scope.ui.connectHost, function () {
 				client.write('GRAPHIC\n');
+			});
+			$scope.ui.connected = true;
+			$timeout(function () {
+				var canvas = document.querySelector('canvas');
+				var engine = new BABYLON.Engine(canvas, true);
+				var scene = new BABYLON.Scene(engine);
+
+				// Creating a camera looking to the zero point (0,0,0), a light, and a sphere of size 1
+				var camera = new BABYLON.ArcRotateCamera('Camera', 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
+				var light = new BABYLON.PointLight('Omni', new BABYLON.Vector3(0, 0, 10), scene);
+				var origin = BABYLON.Mesh.CreateSphere('origin', 10, 1.0, scene);
+
+				scene.activeCamera.attachControl(canvas);
+
+				engine.runRenderLoop(function () {
+					scene.render();
+				});
+
 			});
 		};
 
